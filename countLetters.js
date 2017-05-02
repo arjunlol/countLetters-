7c3ae8456e316b72ab4d6d4a66cc54e85484d4c4
Arjun Lall <lallarjun@gmail.com>
2017-05-02 21:15:16 +0000

    counts letters

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+
+
+
+function countLetters (letters){
+  var countOfLetters = {};
+  letters = letters.split(" ").join("").toLowerCase();
+  for (l of letters){
+    countOfLetters = isItInObject(l, countOfLetters);
+  }
+  return countOfLetters;
+}
+
+
+function isItInObject(letter, lettersObject){
+
+  for (key in lettersObject){
+    if (letter == key){
+      lettersObject[key] = Number(lettersObject[key])+1;
+      return lettersObject;
+    }
+  }
+  lettersObject[letter] = 1;
+  return lettersObject
+}
+console.log(countLetters("lighthouse in the house"));
+//isItInObject("r", {l:"2"});
